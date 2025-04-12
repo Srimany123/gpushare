84426fc2a5bf46ae17524d6d886c6b3a064493b3
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2025-04-12 17:54:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpushare",
-    version="0.1.1",
+    version="0.1.0",
     description="Client library for the GPU Share service",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
